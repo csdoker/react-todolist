1d28d58cb7463bab8348b340fcd1fae13456a83d
csdoker <csd758371536@qq.com>
2021-02-02 20:21:26 +0800

    feat: 使用多个元素的动画效果

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 // import TodoList from './TodoList'
-import Transition from './Transition'
+// import Transition from './Transition'
+import TransitionList from './TransitionList'
 
 ReactDOM.render(
-  <Transition />,
+  <TransitionList />,
   document.getElementById('root')
 )
