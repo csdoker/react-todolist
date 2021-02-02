415f9f8bb7df1366f0b9e0ff0ed1cacbe52e92a3
csdoker <csd758371536@qq.com>
2021-02-02 13:07:09 +0800

    feat: 添加组件接收的参数类型约束

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
 class TodoItem extends Component {
+  constructor (props) {
+    super(props)
+    this.handleClick = this.handleClick.bind(this)
+  }
+
   render () {
-    const { content } = this.props
+    const { content, test } = this.props
     return (
-      <div onClick={this.handleClick.bind(this)}>{content}</div>
+      <div onClick={this.handleClick}>{test} - {content}</div>
     )
   }
 
@@ -12,6 +18,17 @@ class TodoItem extends Component {
     const { deleteItem, index } = this.props
     deleteItem(index)
   }
+}
+
+TodoItem.propTypes = {
+  test: PropTypes.string.isRequired,
+  content: PropTypes.string,
+  deleteItem: PropTypes.func,
+  index: PropTypes.number
+}
+
+TodoItem.defaultProps = {
+  test: 'hello world'
 }
 
 export default TodoItem
