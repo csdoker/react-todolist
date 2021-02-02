a57c36ecccac9592eb98e5b0907d5d5594e71c3b
csdoker <csd758371536@qq.com>
2021-02-02 19:39:00 +0800

    feat: 使用css实现动画效果

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import TodoList from './TodoList'
+// import TodoList from './TodoList'
+import Transition from './Transition'
 
 ReactDOM.render(
   <React.StrictMode>
-    <TodoList />
+    <Transition />
   </React.StrictMode>,
   document.getElementById('root')
 )
