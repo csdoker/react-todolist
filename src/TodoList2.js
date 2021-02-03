40ebf1e557b45c49609e04f92572655af94e542f
csdoker <csd758371536@qq.com>
2021-02-03 12:08:32 +0800

    feat: 完成redux中store模块的编写

--- src/TodoList2.js
+++ src/TodoList2.js
@@ -3,17 +3,22 @@ import React, { Component } from 'react'
 import { Input, Button, List } from 'antd'
 import 'antd/dist/antd.css'
 
-const data = ['内容1', '内容2', '内容3']
+import store from './store'
 
 class TodoList2 extends Component {
+  constructor (props) {
+    super(props)
+    this.state = store.getState()
+  }
+
   render () {
     return (
       <div style={{ margin: '10px' }}>
         <div>
-          <Input placeholder='todo info' style={{ width: '200px', marginRight: '10px' }} />
+          <Input value={this.state.inputValue} placeholder='todo info' style={{ width: '200px', marginRight: '10px' }} />
           <Button type='primary'>提交</Button>
         </div>
-        <List style={{ marginTop: '10px', width: '200px' }} bordered dataSource={data} renderItem={item => (<List.Item>{item}</List.Item>)} />
+        <List style={{ marginTop: '10px', width: '300px' }} bordered dataSource={this.state.list} renderItem={item => (<List.Item>{item}</List.Item>)} />
       </div>
     )
   }
