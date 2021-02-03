5730e17903d620d1ab335acbbd3bd23ed89e9eb1
csdoker <csd758371536@qq.com>
2021-02-03 12:35:27 +0800

    feat: 完成reducer模块的编写

--- src/TodoList2.js
+++ src/TodoList2.js
@@ -9,18 +9,41 @@ class TodoList2 extends Component {
   constructor (props) {
     super(props)
     this.state = store.getState()
+    this.handleInputChange = this.handleInputChange.bind(this)
+    this.handleStoreChange = this.handleStoreChange.bind(this)
+    this.handleButtonClick = this.handleButtonClick.bind(this)
+    store.subscribe(this.handleStoreChange)
   }
 
   render () {
     return (
       <div style={{ margin: '10px' }}>
         <div>
-          <Input value={this.state.inputValue} placeholder='todo info' style={{ width: '200px', marginRight: '10px' }} />
-          <Button type='primary'>提交</Button>
+          <Input value={this.state.inputValue} placeholder='todo info' style={{ width: '200px', marginRight: '10px' }} onChange={this.handleInputChange} />
+          <Button type='primary' onClick={this.handleButtonClick}>提交</Button>
         </div>
         <List style={{ marginTop: '10px', width: '300px' }} bordered dataSource={this.state.list} renderItem={item => (<List.Item>{item}</List.Item>)} />
       </div>
     )
+  }
+
+  handleInputChange (e) {
+    const action = {
+      type: 'changeInputValue',
+      value: e.target.value
+    }
+    store.dispatch(action)
+  }
+
+  handleStoreChange () {
+    this.setState(store.getState())
+  }
+
+  handleButtonClick () {
+    const action = {
+      type: 'addTodoItem'
+    }
+    store.dispatch(action)
   }
 }
 
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -4,6 +4,17 @@ const defaultState = {
 }
 
 const reducer = (state = defaultState, action) => {
+  if (action.type === 'changeInputValue') {
+    const newState = JSON.parse(JSON.stringify(state))
+    newState.inputValue = action.value
+    return newState
+  }
+  if (action.type === 'addTodoItem') {
+    const newState = JSON.parse(JSON.stringify(state))
+    newState.list.push(newState.inputValue)
+    newState.inputValue = ''
+    return newState
+  }
   return state
 }
 
